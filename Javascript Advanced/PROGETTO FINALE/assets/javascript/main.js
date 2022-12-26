37004a332944ea56de9c5ee18106bb003a6511ae
giovicidu@gmail.com <giovicidu@gmail.com>
2022-12-27 00:17:43 +0100

    getData creati correttamente. Utilizzato axios

--- Javascript Advanced/PROGETTO FINALE/assets/javascript/main.js
+++ Javascript Advanced/PROGETTO FINALE/assets/javascript/main.js
@@ -1,53 +1,12 @@
 //-------import Funtions from httpAPI.js-------
-import {getAllNews, getNews} from "./modules/httpAPI.js";
+import {getNews,getTopNewsId,getTopNews} from "./modules/httpAPI.js";
 import {getNewFromID} from "./modules/httpAPI.js";
 //-------import Funtions from utilities.js-------
 
 
-//globali
-let dataId;
-
 //getNewFromID(8863);
 
-//getAllNews();
-let topTenNews= await getNews(10);
+let topTenNews =  await getTopNewsId(10);
+console.clear();
+await getTopNews(7);
 console.log(topTenNews);
-
-/*
-getDataID('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
-.then(res => res.json())
-.then(function(json){
-    dataId=json;
-    console.log(dataId);
-})
-.then(function(res){
-    
-    
-});
-
-*/
-
-/*
-getDataID('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
-.then(res => res.json())
-.then(function(json){
-    dataId=json;
-    console.log(dataId);
-    /*let title = data.products[1].title;
-    let desc = data.products[1].description;
-    let p1 = createP(title,"mb-2 mr-6 ml-6");
-    let p2 = createP(desc,"mb-6 mr-6 ml-6");
-    let title2 = data.products[2].title;
-    let desc2 = data.products[2].description;
-    let p12 = createP(title2,"mb-2 mr-6 ml-6");
-    let p22 = createP(desc2,"mb-6 mr-6 ml-6");
-    per andare altra riga
-    appendElementToADiv(divMain,p1);
-    appendElementToADiv(divMain,p2)
-    appendElementToADiv(divMain1,p12);
-    appendElementToADiv(divMain1,p22)
-    appendElementToADiv(divMain,p1);
-    appendElementToADiv(divMain,p2)
-    appendElementToADiv(divMain,p12);
-    appendElementToADiv(divMain,p22)
-})*/
--- Javascript Advanced/PROGETTO FINALE/assets/javascript/modules/httpAPI.js
+++ Javascript Advanced/PROGETTO FINALE/assets/javascript/modules/httpAPI.js
@@ -1,55 +1,67 @@
-const urlOfData='https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty';
+const urlOfData = "https://hacker-news.firebaseio.com/v0/newstories.json" // URL dalla quale estrapolare i dati
+
+//----------------Variabili globali----------------
+let dataOfTopTen;
+let datas = new Array;
+let dataOfElementInCache;
+
 
 export async function getNewFromID(itemNumber){
     const URL='https://hacker-news.firebaseio.com/v0/item/'+itemNumber+'.json?print=pretty';
-    let dataOfTheNew;
-    fetch(URL)
-    .then(res => res.json())
-    .then(function(json){
-        dataOfTheNew=json;
-        console.log('Job done with item: ' + itemNumber );
-        console.log(dataOfTheNew);
-        return dataOfTheNew;
-    });
+    let newsElement = await axios.get(URL)
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+        dataOfElementInCache=response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
 }
 
+export async function getNews(){
+    let newsElement = await axios.get(urlOfData)
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+        return response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+}
 
-async function setNewsInMap(newsAsObject,numberOfNews){
-    let myMapWithNews = new Map();
-    
-    for(let i=0;i<numberOfNews;i++) {
-        let idOfTheNew=newsAsObject[i];//id of the News
-        myMapWithNews.set(i,getNewFromID(idOfTheNew));
+export async function getTopNews(numberOfNews){
+    for(let i=0;i<numberOfNews;i++){
+        await getNewFromID(dataOfTopTen[i]);
+        //console.log(dataOfElementInCache);
+        datas.push(dataOfElementInCache);//id of the News
     }
-    console.log('La mappa degli oggetti è : '+ myMapWithNews);
-    console.log(myMapWithNews.values());
-    return myMapWithNews;
+    console.log(datas);
+    
 }
 
-export async function getNews(numberOfNews){
-    let news;
-    let mapOfNews;
-        fetch(urlOfData)
-        .then(res => res.json())
-        .then(function(json){
-            news=json;
-            mapOfNews = setNewsInMap(news,numberOfNews);
-            return mapOfNews;
-        })
-        .catch(e=>{
-            console.log('Error type: '+e);
-        })
+export async function getTopNewsId(numberOfNews){
+    await getTopNewsFromApi(numberOfNews); //attendi che si setti l'array con le informazioni
+    return dataOfTopTen;
 }
 
-export async function getAllNews(){
-    let news;
-    fetch(urlOfData)
-     .then(res => res.json())
-     .then(function(json){
-        news=json;
-        console.log(news);
-        console.log(typeof news)
-       return news;
-     });
-}
-
+async function getTopNewsFromApi(numberOfNews){
+    //----------------Prendi tutti i 500 dati----------------
+    let newsElement= await axios.get(urlOfData) 
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+        let array=new Array;
+        for(let i=0;i<numberOfNews;i++) {
+            array.push(response.data[i]);//id of the News
+        }
+        dataOfTopTen=array;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+}   
