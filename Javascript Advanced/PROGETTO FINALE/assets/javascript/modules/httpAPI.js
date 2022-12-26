b3bdde5130e97558a098d3884308fd3749a2558a
giovicidu@gmail.com <giovicidu@gmail.com>
2022-12-26 14:15:24 +0100

    getNews ans getNewFromID

--- Javascript Advanced/PROGETTO FINALE/assets/javascript/modules/httpAPI.js
+++ Javascript Advanced/PROGETTO FINALE/assets/javascript/modules/httpAPI.js
@@ -1,12 +1,55 @@
-export async function getDataID(URL){
-    return fetch(URL);
+const urlOfData='https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty';
+
+export async function getNewFromID(itemNumber){
+    const URL='https://hacker-news.firebaseio.com/v0/item/'+itemNumber+'.json?print=pretty';
+    let dataOfTheNew;
+    fetch(URL)
+    .then(res => res.json())
+    .then(function(json){
+        dataOfTheNew=json;
+        console.log('Job done with item: ' + itemNumber );
+        console.log(dataOfTheNew);
+        return dataOfTheNew;
+    });
 }
 
-export async function getData(URL){
-    return fetch(URL);
+
+async function setNewsInMap(newsAsObject,numberOfNews){
+    let myMapWithNews = new Map();
+    
+    for(let i=0;i<numberOfNews;i++) {
+        let idOfTheNew=newsAsObject[i];//id of the News
+        myMapWithNews.set(i,getNewFromID(idOfTheNew));
+    }
+    console.log('La mappa degli oggetti è : '+ myMapWithNews);
+    console.log(myMapWithNews.values());
+    return myMapWithNews;
 }
 
-export async function getDataNews(URL){
-    return fetch(URL);
+export async function getNews(numberOfNews){
+    let news;
+    let mapOfNews;
+        fetch(urlOfData)
+        .then(res => res.json())
+        .then(function(json){
+            news=json;
+            mapOfNews = setNewsInMap(news,numberOfNews);
+            return mapOfNews;
+        })
+        .catch(e=>{
+            console.log('Error type: '+e);
+        })
+}
+
+export async function getAllNews(){
+    let news;
+    fetch(urlOfData)
+     .then(res => res.json())
+     .then(function(json){
+        news=json;
+        console.log(news);
+        console.log(typeof news)
+       return news;
+     });
 }
 
