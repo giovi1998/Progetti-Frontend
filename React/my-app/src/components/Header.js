cacbb7ce091dd188c84e8e50542d905dc9f13ba8
giovicidu@gmail.com <giovicidu@gmail.com>
2023-01-02 15:36:00 +0100

    Take code from https://www.youtube.com/watch?v=w7ejDZ8SWv8

--- React/my-app/src/components/Header.js
+++ React/my-app/src/components/Header.js
@@ -1,21 +1,36 @@
-//Took from the extension ES7 React/Redux/GraphQL/React-Native snippets v1.9.3
-//write rafce, impt
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   )
 }
 
-Header.defaultProps={
-    title: 'Ciao Sono il titolo di default'
+Header.defaultProps = {
+  title: 'Task Tracker',
 }
 
-Header.propTypes={
-    title: PropTypes.string.isRequired,
-
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
 export default Header
